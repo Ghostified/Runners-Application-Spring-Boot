222a8168587a498f1cc5209e1e4dc35d8d1e92ff
Ghostified <bransonallan@gmail.com>
2024-08-16 23:56:57 +0300

    completed the basic rest api, changed port to 8085

--- src/main/java/dev/Branson/RunnersAppSpringBoot/Run/Run.java
+++ src/main/java/dev/Branson/RunnersAppSpringBoot/Run/Run.java
@@ -12,13 +12,16 @@ public record Run(
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive //constraint to prevent negative integer input for miles
         Integer miles,
-        @Positive //prevent negative integer input for miles
         Location location
 ) {
     public  Run {
         if(!completedOn.isAfter(startedOn)) {
             throw  new IllegalArgumentException("Completed On must be after started on");
+        }
+        if(title.isEmpty()){
+            System.out.println("The created run does not have a title");
         }
     }
 }
--- src/main/java/dev/Branson/RunnersAppSpringBoot/Run/RunController.java
+++ src/main/java/dev/Branson/RunnersAppSpringBoot/Run/RunController.java
@@ -81,7 +81,7 @@ public class RunController {
     //PUT,UPDATE
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run,@PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run,@PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
